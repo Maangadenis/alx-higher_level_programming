e90743d5eaaadb4fcb9052304ca54696f7cd5bba
unknown <denismaanga99@gmail.com>
2022-06-27 14:38:39 +0300

    Rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -72,4 +72,71 @@ class Rectangle:
         """
             setter function for the private attribute height
             Args:
+value (int): new height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
+    def area(self):
+        """
+            public instance method to calculate area of rectangle
+            Returns: area of rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+            public instance method to calculate the perimeter of a rectangle
+            Returns: perimeter of rectangle
+        """
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """
+            return string representation of a rectangle
+        """
+        rectangle = ""
+        if self.__width is 0 or self.__height is 0:
+            return rectangle
+
+        for i in range(self.__height - 1):
+            rectangle += str(self.print_symbol) * self.__width + "\n"
+        rectangle += str(self.print_symbol) * self.__width
+
+        return rectangle
+
+    def __repr__(self):
+        """
+            returns a string representation of the rectangle to be
+            able to recreate a new instance
+        """
+        rectangle = "Rectangle({}, {})".format(self.__width, self.__height)
+        return rectangle
+
+    def __del__(self):
+        """
+            properly deletes the instance of a class.
+        """
+        if Rectangle.number_of_instances > 0:
+            Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        area_1 = rect_1.__width * rect_1.__height
+        area_2 = rect_2.width * rect_2.height
+        if area_1 > area_2:
+            return rect_1
+        elif area_2 > area_1:
+            return rect_2
+        else:
+            return rect_1
