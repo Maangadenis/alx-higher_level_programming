197790388c78baeca13c8ebe9e20764a76ef6dd4
unknown <denismaanga99@gmail.com>
2022-07-05 14:48:21 +0300

    Tasak 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""
-    This is a python script that adds all arguments to a Python List.
-    List is then saved to a file.
-"""
+"""script"""
 
-import sys
+
+from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+try:
+    elems_list = load_from_json_file("add_item.json")
+except:
+    elems_list = []
 
-if __name__ == "__main__":
-
-    filename = "add_item.json"
-
-    try:
-        arg_list = load_from_json_file(filename)
-    except:
-        arg_list = []
-
-    for arg in sys.argv[1:]:
-        arg_list.append(arg)
-    save_to_json_file(arg_list, filename)
+for elements in (argv[1:]):
+    elems_list.append(elements)
+    save_to_json_file(elems_list, "add_item.json")
