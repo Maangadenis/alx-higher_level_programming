a70b07e03af4ed491ba028325e0d0d605eeb9ad4
unknown <denismaanga99@gmail.com>
2022-07-05 14:58:46 +0300

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""script"""
+"""This is the documentation for this module. On this file we have to
+write a JSON rep of an object to a file"""
 
 
-from sys import argv
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-try:
-    elems_list = load_from_json_file("add_item.json")
-except:
-    elems_list = []
-
-for elements in (argv[1:]):
-    elems_list.append(elements)
-    save_to_json_file(elems_list, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as archivito:
+        import json
+        atta = json.dumps(my_obj)
+        archivito.write(atta)
