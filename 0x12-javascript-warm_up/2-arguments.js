f788663b3cd1404307996ec9190c81fc26e5c42b
unknown <denismaanga99@gmail.com>
2022-09-05 14:45:15 +0300

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,13 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+
+// Script that prints "JavaScript is amazing".
+
+if (process.argv.length === 2) {
+  console.log('No argument');
+}
+if (process.argv.length === 3) {
+  console.log('Argument found');
+}
+if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
